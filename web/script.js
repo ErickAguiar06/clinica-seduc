54ac9bfbd959ba858900e14b19a858adc88a13c9
ErickAguiar06 <erick.aguiar06@gmail.com>
2025-03-12 11:29:40 -0300

    atts

--- web/script.js
+++ web/script.js
@@ -2,12 +2,14 @@ const uri = 'http://localhost:3000';
 const titulo = document.querySelector('header h1');
 const main = document.getElementById('cards');
 
+//Obtendo o título da API para a tela (READ)
 fetch(uri)
     .then(resp => resp.json())
     .then(resp => {
         titulo.innerHTML = resp.titulo
     })
 
+//Obtendo da API e listando as consultas em forma de CARDS (READ)
 fetch(uri + '/consultas')
     .then(resp => resp.json())
     .then(resp => {
@@ -20,14 +22,15 @@ fetch(uri + '/consultas')
             <p>Data: <label contenteditable=true>${new Date(consulta.data_hora).toLocaleDateString('pt-br')}</label></p>
             <p>Hora: <label contenteditable=true>${consulta.data_hora.split('T')[1].substring(0, 5)}</label></p>
             <div>
-            <button onClick="deletar(${consulta.consulta.id})"> - </button>
-            <button onClick="alterar(${consulta.consulta.id})"> * </button>
+                <button onclick="deletar(${consulta.consulta_id})">-</button>
+                <button onclick="alterar(${consulta.consulta_id},this)">*</button>
             </div>
             `;
             main.appendChild(card);
         });
     })
 
+//Cadastrando nova conculta (CREATE)
 const form = document.querySelector('#cadastro form')
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -48,21 +51,45 @@ form.addEventListener('submit', e => {
                 window.location.reload();
             else
                 alert('Erro ao enviar dados para a API');
-        }).catch(
-            alert('Sem concexão com o Servidor')
-        )
+        })
 })
 
-function deletar(consulta_id) {
-    fetch(uri + `/consultas/${consulta_id}`, {
-        method:'DELETE'
-    })
-    .then(resp => resp.status)
-    .then(resp => {
-        if(resp ==204)
-            
-            window.location.reload();
-        else
-        alert('Erro ao enviar dadospara a API');
-    })
+//Alterando dados da consulta (UPDATE)
+function alterar(id, e) {
+    let data = e.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML;
+    let hora = e.parentNode.parentNode.childNodes[9].childNodes[1].innerHTML;
+    let dataBD = data.split('/').reverse().toString()
+    let dataHora = dataBD.replaceAll(',', '-') + 'T' + hora + ':00.000Z'
+    const dados = {
+        paciente: e.parentNode.parentNode.childNodes[3].childNodes[1].innerHTML,
+        medico: e.parentNode.parentNode.childNodes[5].childNodes[1].innerHTML,
+        quando: dataHora
+    }
+    const cabecalho = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+    }
+    fetch(uri + '/consultas/' + id, cabecalho)
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 202) {
+                window.location.reload();
+            } else {
+                alert("Erro ao alterar");
+            }
+        })
+}
+
+//Excluído uma consulta (DELETE)
+function deletar(id) {
+    fetch(uri + '/consultas/' + id, { method: 'DELETE' })
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 204) {
+                window.location.reload();
+            } else {
+                alert('Erro ao excluir');
+            }
+        })
 }
